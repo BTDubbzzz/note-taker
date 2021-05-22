22778cd10c5464ba7a5fc1a1f08356fbce011c6d
BTDubbzzz <btdubbzzz@gmail.com>
2021-05-22 18:01:10 -0500

    added express.static to server static files

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const express = require('express');
 const app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('Develop/public'));
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
